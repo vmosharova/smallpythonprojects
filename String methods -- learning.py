cab289ddb1eb48f1048280d1ed4f6c737927990c
Varvara Mosharova <varya.mosharova@gmail.com>
2021-06-14 23:05:57 +0300

    String methods: Finding duplicates in a str an other excercises

--- String methods -- learning.py
+++ String methods -- learning.py
@@ -1,6 +1,6 @@
 #Palindrome
 
-s = 'lorem ipsum'
+s = ' lorem ipsum '
 
 def simple_reversed(s):
     return (s[::-1]) #faster approach
@@ -16,3 +16,27 @@ def is_palindrome(s):
 
 print(is_palindrome(s)) #False
 print(is_palindrome('abcba')) #True
+
+
+#Return the string without any whitespace at the beginning or the end
+print(s.strip())
+
+#Convert the value of txt to upper case.
+s = 'lorem ipsum'
+print(s.upper())
+
+#Make the first letter the upper case letter
+print(s.capitalize())
+
+# Write a program to count the number of characters (character frequency) in a string.
+def number_char(s):
+    a = dict()
+    for i in s:
+        keys = a.keys()
+        if i in keys:
+            a[i] += 1
+        else:
+            a[i] = 1
+    return a
+
+print(number_char(s))
