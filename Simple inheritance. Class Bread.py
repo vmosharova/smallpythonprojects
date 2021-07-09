cd93c2697ba631a25a190847f6ac2f8ef19465ef
Varvara Mosharova <varya.mosharova@gmail.com>
2021-07-09 17:01:59 +0300

    Classes: modified simple inheritance Brötchen(Bread)

--- Simple inheritance. Class Bread.py
+++ Simple inheritance. Class Bread.py
@@ -14,11 +14,37 @@ class Bread:
         self.price = price
 
     def __repr__(self):
-        return f'The product weight is {self.weight} and the price is {self.price}'
+        return f'The bread weight is {self.weight} and the price is {self.price}'
 
     def price_for_100g(self):
-        return (self.price * 100) / self.weight
+        price = (self.price * 100) / self.weight
+        return '100g of product cost ' + str(price)
 
 
 class Broetchen(Bread):
-    pass
+
+    def __init__(self, weight, price, stuffing_weight):
+        Bread.__init__(self, weight, price)
+        self.stuffing_weight = stuffing_weight
+
+    def __repr__(self):
+        return Bread.__repr__(self) + f' and the stuffing weight is {self.stuffing_weight}'
+
+    def price_for_100g(self):
+        price = (self.price * 100) / (self.weight + self.stuffing_weight)
+        return '100g of product cost ' + str(price)
+
+    def stuffing_concentration(self):
+        concentration = (self.stuffing_weight / (self.weight + self.stuffing_weight)) * 100
+        return 'The stuffing concentration is ' + str(concentration) + '%'
+
+bread = Bread(800, 50)
+broetchen = Broetchen(125, 45, 75)
+
+print(bread)
+print(broetchen)
+print(bread.price_for_100g())
+print(broetchen.price_for_100g())
+print(broetchen.stuffing_concentration())
+
+
